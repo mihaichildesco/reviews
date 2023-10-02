6d54a19d4034dfee8bc21956b1ed922289684853
Mihai Childesco <mihai.childesco@gmail.com>
2023-10-02 19:25:59 +0300

    add logic js and randomPerson

--- app.js
+++ app.js
@@ -40,3 +40,43 @@ const nextBtn = document.querySelector('.next-btn');
 const randomBtn = document.querySelector('.random-btn');
 
 // set starting item
+let currentItem = 0;
+
+// load initial item
+
+window.addEventListener('DOMContentLoaded', function () {
+  showPerson(currentItem);
+});
+
+randomBtn.addEventListener('click', function () {
+  let randomPerson = Math.floor(Math.random() * reviews.length);
+  showPerson(randomPerson);
+});
+
+// show person based on item
+
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show nest person
+nextBtn.addEventListener('click', function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+// show prev person
+prevBtn.addEventListener('click', function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
